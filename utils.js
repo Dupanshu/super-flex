a1ac5e03c235f11301616cf684b1bf0184301a16
Dupanshu <124947066+Dupanshu@users.noreply.github.com>
2024-03-07 22:49:24 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,18 +1,22 @@
 'use strict';
 
 // Get HTML element by id
-export function getElement(selector, scope = document) {
-    return scope.getElementById(selector);
+export function getElementById(selector, scope = document) {
+    return scope.getElementById(sel);
   }
   
   // Select HTML element
   export function select(selector, scope = document) {
-    return scope.querySelector(selector);
+    return scope.querySelector(sel);
   }
   
   // Get a list of HTML elements as an array
-  export function selectAll(selector, scope = document) {
-    return [...scope.querySelectorAll(selector)];
+  export function selectAllEl(selector, scope = document) {
+    return [...scope.querySelectorAll(sel)];
   }
-  
 
+  export function print(...args) {
+    args.forEach(element => {
+        console.log(element);
+      });
+  }
